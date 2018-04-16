b51eb6b656cb61591bb566b4ac5ee81a990824f1
ivangsp <talk2gspivan@outlook.com>
2018-04-16 07:52:59 +0300

    add dynamic ip address

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var path = require('path');
 var fs = require('fs');
 const express = require('express');
 var Jimp = require("jimp");
-const { performance } = require('perf_hooks');
+const  {performance}  = require('perf_hooks');
 
 
 
@@ -65,21 +65,22 @@ app.post('/post', function(req, res){
 });
 
 
-app.listen(port);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
+// var n = require('os').networkInterfaces()
 
-var n = require('os').networkInterfaces()
-
-var myIp = module.exports = function () {
-    var ip = []
-    for(var k in n) {
-    var inter = n[k]
-    for(var j in inter)
-        if(inter[j].family === 'IPv4' && !inter[j].internal)
-        return inter[j].address
-    }
-}
-var ipString = String(myIp());
+// var myIp = module.exports = function () {
+//     var ip = []
+//     for(var k in n) {
+//     var inter = n[k]
+//     for(var j in inter)
+//         if(inter[j].family === 'IPv4' && !inter[j].internal)
+//         return inter[j].address
+//     }
+// }
+// var ipString = String(myIp());
 
 
-console.log('Server IP address:' + ipString + ':' + port);
+// console.log('Server IP address:' + ipString + ':' + port);
 
